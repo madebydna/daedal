5aefe008194bb66aaf4cff0eb27f7b2d23f4344e
Andrea Singh <info@madebydna.com>
2018-01-08 15:10:03 -0800

    Added boost option to nested query

--- lib/daedal/queries/nested_query.rb
+++ lib/daedal/queries/nested_query.rb
@@ -2,18 +2,19 @@ module Daedal
   module Queries
     """Class for the bool query"""
     class NestedQuery < Query
-  
+
       # required attributes
       attribute :path,        Daedal::Attributes::Field
       attribute :query,       Daedal::Attributes::Query
-  
+
       # non required attributes
       attribute :score_mode,  Daedal::Attributes::ScoreMode,  required: false
       attribute :name,        Symbol,                         required: false
-  
+      attribute :boost,       Daedal::Attributes::Boost,      required: false
+
       def to_hash
         result = {nested: {path: path, query: query.to_hash}}
-        options = {score_mode: score_mode, _name: name}.select { |k,v| !v.nil? }
+        options = {score_mode: score_mode, _name: name, boost: boost}.select { |k,v| !v.nil? }
         result[:nested].merge! options
 
         result
--- spec/unit/daedal/queries/nested_query_spec.rb
+++ spec/unit/daedal/queries/nested_query_spec.rb
@@ -72,6 +72,28 @@ describe Daedal::Queries::NestedQuery do
     end
   end
 
+  context 'with boost specified' do
+    let(:query) do
+      subject.new query: match_all_query, path: :foo, boost: 5
+    end
+    let(:hash_query) do
+      {nested: {path: :foo, query: match_all_query.to_hash, boost: 5.0}}
+    end
+
+    it 'will create a nested query with the appropriate fields' do
+      expect(query.query).to eq match_all_query
+      expect(query.path).to eq :foo
+      expect(query.boost).to eq 5.0
+    end
+
+    it 'will have the correct hash representation' do
+      expect(query.to_hash).to eq hash_query
+    end
+    it 'will have the correct json representation' do
+      expect(query.to_json).to eq hash_query.to_json
+    end
+  end
+
   context 'with an invalid score_mode specified' do
     it 'will raise an error' do
       expect{subject.new(query: match_all_query, path: :foo, score_mode: 'foo')}.to raise_error(Virtus::CoercionError)
